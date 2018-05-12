18f1a69cfe5d2fc0ace0260e830dabbc1ba61d9e
fishcg <353740902@qq.com>
2018-05-12 18:12:33 +0800

    规范代码格式

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//app.js
- //var bgmusic ="http://m2.music.126.net/5W0FdIuDE5ypFTpBtMVjfQ==/18550960185473716.mp3";
 const CACHE = require("/js/constants/cache_key.js");
 const KEY = require("/js/constants/key.js");
 const MUtils = require("/js/MUtils.js");
@@ -9,42 +7,42 @@ App({
   CACHE: CACHE,
   KEY: KEY,
   onLaunch: function () {
-    //调用API从本地缓存中获取数据在
+    // 调用 API 从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
     // 启动音
     const backgroundAudioManager = wx.getBackgroundAudioManager()
-
     backgroundAudioManager.title = '喵~'
     backgroundAudioManager.epname = 'i音声启动音'
     backgroundAudioManager.singer = '加特林'
     backgroundAudioManager.coverImgUrl = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
     backgroundAudioManager.src = 'http://static.missevan.com/sound/201605/14/297966dceaf85625530bad6e682dda47120400.mp3'
-    
   },
+
   onHide: function() {
     // Do something when page hide.
   },
 
-  /*
-  *格式化时间戳
-  *
-  *@todo 考虑加入常用工具函数类中
-  *@params string format 时间格式
-  *@params string timestamp 时间戳
-  *@params string  格式化后的时间
-  */
+  /**
+   *格式化时间戳
+   *
+   * @todo 考虑加入常用工具函数类中
+   * @params {String} format 时间格式
+   * @params {String} timestamp 时间戳
+   * @params {String} 格式化后的时间
+   */
   date:function(format, timestamp){
     return MUtils.date(format, timestamp)
   },
   ajax: function (info, check_login) {
     return Request.ajax(info, check_login)
   },
-  /*
-  *登陆
-  *@params 登陆成功后执行的方法
-  */
+
+  /**
+   * 登陆
+   * @params 登陆成功后回调方法
+   */
   getUser: function(format, timestamp) {
     return User.getUser(format, timestamp)
   },
@@ -52,7 +50,8 @@ App({
   audioPlay:function (e) {
    e.play()
   },
-  globalData:{
+
+  var globalData = {
     postUrl: KEY.DOMAIN,
     userInfo:null,
     bgmusic:"",
--- js/constants/key.js
+++ js/constants/key.js
@@ -1,10 +1,9 @@
-/*
+/**
  *定义小程序所使用的常量
  *created by fish
  *2018-02-03
-*/
+ */
 // 访问域名
-// export const DOMAIN = 'https://www.acgay.cn/'
-export const DOMAIN = 'http://www.ggg.com/'
+export const DOMAIN = 'https://www.text.com/'
 export const NOT_LOGIN = 10001
 export const FAIL = 10002
